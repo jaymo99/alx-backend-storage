7ea413daa2c7432782ba4a7a14bbeb989d06a318
James Kariuki <karisjaymo99@gmail.com>
2024-05-08 16:51:09 +0000

    [bugfix] Use semicolons to terminate statements

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -10,8 +10,8 @@ BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF !(NEW.email <=> OLD.email) THEN
-        SET NEW.valid_email = 0
-    END IF
+        SET NEW.valid_email = 0;
+    END IF;
 END;
 //
 
