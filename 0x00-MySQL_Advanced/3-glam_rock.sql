05e47c1cc0e930864916c4ba71b632e8b7a053af
James Kariuki <karisjaymo99@gmail.com>
2024-05-08 16:04:04 +0000

    [bugfix] Use 2022 for bands that didn't split

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,11 @@
 -- List all bands with `Glam rock` as their main style
 -- Ranked by longevity
-SELECT band_name, (2022 - formed) AS lifespan
+SELECT
+    band_name,
+    CASE
+        WHEN split IS NULL THEN (2022 - formed)
+        ELSE split - formed
+    END AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
