f1978135408433da277796fc21dc5fbefedf337e
James Kariuki <karisjaymo99@gmail.com>
2024-05-14 08:43:12 -0700

    Task 8, List mongodb documents in python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+MongoDB module. Defines function to list all documents in a collection
+"""
+
+def list_all(mongo_collection):
+    """
+    Returns a list of all documents in a collection
+    """
+    return mongo_collection.find()
